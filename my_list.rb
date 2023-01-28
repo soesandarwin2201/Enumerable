33e2836f0e63edff086265f7bccd73153191e3fa
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-01-28 10:35:19 +0630

    Check the linters

--- my_list.rb
+++ my_list.rb
@@ -1,22 +1,23 @@
 require_relative 'myenumerable'
 
 class MyList
-include Myenumerable
- def initialize(*list)
-  @list = list
- end
+  include Myenumerable
+  def initialize(*list)
+    @list = list
+  end
 
- def each(&block)
-  @list.each(&block)
- end
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
-list = MyList.new(1,2,3,4)
-puts(list.all? { |n| n < 5 } )
+list = MyList.new(1, 2, 3, 4)
 
-puts(list.all? { |n| n > 5 } )
+puts(list.all? { |n| n < 5 })
 
-puts(list.any? { |n|n == 2 })
+puts(list.all? { |n| n > 5 })
+
+puts(list.any? { |n| n == 2 })
 
 puts(list.any? { |n| n == 5 })
 
