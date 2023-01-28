321703dfeb99dfa10c9449883d6cddf95c17a934
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-01-28 10:26:14 +0630

    Create the class

--- my_list.rb
+++ my_list.rb
@@ -6,5 +6,18 @@ include Myenumerable
   @list = list
  end
 
- 
+ def each(&block)
+  @list.each(&block)
+ end
 end
+
+list = MyList.new(1,2,3,4)
+puts(list.all? { |n| n < 5 } )
+
+puts(list.all? { |n| n > 5 } )
+
+puts(list.any? { |n|n == 2 })
+
+puts(list.any? { |n| n == 5 })
+
+puts(list.filter(&:even?))
--- myenumerable.rb
+++ myenumerable.rb
@@ -1,17 +1,17 @@
 module Myenumerable
  def all?(&block)
-  each { |n| return false unless yeild(n) }
+  each { |n| return false unless block.call(n) }
   true
  end
 
  def any?(&block)
-  each { |n| return true if yeild(n) }
+  each { |n| return true if block.call(n) }
   false
  end
 
  def filter(&block)
   result = []
-  each { |n| result.push(n) if yeild(n)}
-  result
+  each { |n| result.push(n) if block.call(n) }
+ result
  end
 end
